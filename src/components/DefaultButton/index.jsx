e2cfe46568a4d37903c452a58841fac1182dbe78
Vitor Galdino <httpvittor@gmail.com>
2022-11-15 20:12:51 -0300

    feat: created DefaultButton component

---
+++ src/components/DefaultButton/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./index.css"
+
+const DefaultButton = ({ value }) => {
+    return <button className='default-button'>{value}</button>;
+}
+
+export default DefaultButton;
