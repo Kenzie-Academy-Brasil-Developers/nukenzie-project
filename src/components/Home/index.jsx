8cff4cedd96ba6054d6dfe8b5e756c028fb4e428
Vitor Galdino <httpvittor@gmail.com>
2022-11-15 23:40:00 -0300

    refactor: replace button component to button tag and import your style

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import DefaultButton from '../DefaultButton';
 import MainImage from '../HomeImage';
 import WhiteLogo from '../WhiteLogo';
-import "./index.css"
+import "../../styles/buttons.css";
+import "./index.css";
 
 const Home = ({ setIsLogged }) => {
-
-    const isLoggedBody = () => setIsLogged(false);
 
     return (
         <>
@@ -17,10 +15,7 @@ const Home = ({ setIsLogged }) => {
                         <WhiteLogo />
                         <h2>Centralize o controle das suas finanças</h2>
                         <span>de forma rápida e segura</span>
-                        <DefaultButton 
-                        value='Iniciar'
-                        target={isLoggedBody}
-                        />
+                        <button className='default-button' onClick={() => setIsLogged(false)}>Iniciar</button>
                     </section>
                     <section className="right">
                         <MainImage />
