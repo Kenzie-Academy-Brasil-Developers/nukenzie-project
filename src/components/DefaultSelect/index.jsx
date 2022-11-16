ecdeea1f330d5ef6c762f8d5d495188fc4c062d7
Vitor Galdino <httpvittor@gmail.com>
2022-11-16 16:32:44 -0300

    feat: created component structure

--- src/components/DefaultSelect/index.jsx
+++ src/components/DefaultSelect/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const DefaultSelect = () => {
-    return ( 
+const DefaultSelect = ({ mainValue, name, children }) => {
+    return (
         <>
-        
+            <select name={name} id={name} required>
+                <option selected disabled value>{mainValue}</option>
+                {children}
+            </select>
         </>
-     );
+    );
 }
- 
+
 export default DefaultSelect;
