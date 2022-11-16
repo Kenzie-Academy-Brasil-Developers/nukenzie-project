549a234167dfc612084caee0283d6aa447c63228
Vitor Galdino <httpvittor@gmail.com>
2022-11-16 15:06:38 -0300

    refactor: call the button component with your props and children

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import MainImage from '../HomeImage';
 import WhiteLogo from '../WhiteLogo';
-import "../../styles/buttons.css";
-import "./index.css";
+import DefaultButton from '../DefaultButton';
+import './index.css';
 
 const Home = ({ setIsLogged }) => {
 
@@ -11,13 +11,15 @@ const Home = ({ setIsLogged }) => {
             <div className='background'>
                 <div className='main-content container'>
 
-                    <section className="left">
+                    <section className='left'>
                         <WhiteLogo />
                         <h2>Centralize o controle das suas finanças</h2>
                         <span>de forma rápida e segura</span>
-                        <button className='default-button' onClick={() => setIsLogged(false)}>Iniciar</button>
+                        <DefaultButton value='Iniciar'>
+                            {() => setIsLogged(false)}
+                        </DefaultButton>
                     </section>
-                    <section className="right">
+                    <section className='right'>
                         <MainImage />
                     </section>
 
