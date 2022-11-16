9271f0e34b3f3c7fb1ac59463a9c197e24f4e713
Vitor Galdino <httpvittor@gmail.com>
2022-11-16 16:48:17 -0300

    feat: import css input style

--- src/components/DefaultSelect/index.jsx
+++ src/components/DefaultSelect/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../../styles/inputs.css';
 
 const DefaultSelect = ({ mainValue, name, children }) => {
     return (
         <>
-            <select name={name} id={name} >
+            <select className='default-select' name={name} id={name} >
                 <option selected disabled>{mainValue}</option>
                 {children}
             </select>
