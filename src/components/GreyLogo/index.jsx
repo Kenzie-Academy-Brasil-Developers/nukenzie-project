8ba339b0ad2f62da8c86b155350258ee532e1282
Vitor Galdino <httpvittor@gmail.com>
2022-11-15 21:09:35 -0300

    feat: created logo components

---
+++ src/components/GreyLogo/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../assets/grey-logo.svg';
+
+const GreyLogo = () => {
+    return ( 
+        <>
+        <img draggable="false" src={logo} alt="Nu Kenzie logo" />
+        </>
+     );
+}
+ 
+export default GreyLogo;
---
+++ src/components/WhiteLogo/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../assets/white-logo.svg';
+
+const WhiteLogo = () => {
+    return ( 
+        <>
+        <img draggable="false" src={logo} alt="Nu Kenzie logo" />
+        </>
+     );
+}
+ 
+export default WhiteLogo;
