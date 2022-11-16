b6b664082829a224b04fdd7909228fe33a72dda9
Vitor Galdino <httpvittor@gmail.com>
2022-11-16 16:43:32 -0300

    feat: final structure of DefaultSelect component

--- src/components/DefaultSelect/index.jsx
+++ src/components/DefaultSelect/index.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const DefaultSelect = ({ mainValue, name, children }) => {
     return (
         <>
-            <select name={name} id={name} required>
-                <option selected disabled value>{mainValue}</option>
+            <select name={name} id={name} >
+                <option selected disabled>{mainValue}</option>
                 {children}
             </select>
         </>
