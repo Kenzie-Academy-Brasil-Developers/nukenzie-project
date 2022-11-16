3d90a8aa6e6707ddb5cc9c4abe7c810bf9c4df21
Vitor Galdino <httpvittor@gmail.com>
2022-11-16 17:16:28 -0300

    feat: final Form component structure

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import DefaultButton from '../DefaultButton';
 import DefaultInput from '../DefaultInput';
+import DefaultSelect from '../DefaultSelect';
+import './index.css'
 
 const Form = () => {
     return (
         <>
-            <div className="main-form">
-                <DefaultInput
-                    type='text'
-                    placeHolder='Digite aqui sua descrição'
-                    value='description'
-                    label='Descrição'
-                    helperText='Ex: Compra de roupas'
-                />
+            <div className='main-form'>
+                <form>
+                    <DefaultInput
+                        type='text'
+                        placeHolder='Digite aqui sua descrição'
+                        value='description'
+                        label='Descrição'
+                        helperText='Ex: Compra de roupas'
+                    />
+                    <div className='footer-form'>
+                        <DefaultInput
+                            type='number'
+                            placeHolder='Valor'
+                            value='value'
+                            label='Valor'
+                            image='real'
+                        />
+
+                        <DefaultSelect mainValue='Tipo de valor' name='type'>
+                            <option value="entrada">Entrada</option>
+                            <option value="saida">Saída</option>
+                        </DefaultSelect>
+                    </div>
+                    
+                    <DefaultButton type='submit' value='Inserir valor'></DefaultButton>
+                </form>
             </div>
         </>
     );
