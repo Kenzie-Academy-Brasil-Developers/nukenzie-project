92679d3d389e25419549ff1e672409665307c807
Vitor Galdino <httpvittor@gmail.com>
2022-11-16 15:54:46 -0300

    feat: call Form component and initial base structure

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Form from '../Form';
 import Header from '../Header';
 
 const Dashboard = ({ setIsLogged }) => {
     return (
         <>
             <Header target={setIsLogged} />
+            <Form/>
         </>
     );
 }
