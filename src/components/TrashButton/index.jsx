50107d8576d9442333af388d4efb8d8963be92a6
Vitor Galdino <httpvittor@gmail.com>
2022-11-16 19:33:53 -0300

    feat: remove value prop of component

--- src/components/TrashButton/index.jsx
+++ src/components/TrashButton/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../../styles/buttons.css';
 
-const TrashButton = ({type = 'button', value, children}) => {
-    return ( <button type={type} className='trash-button' onClick={children}>{value}</button> );
+const TrashButton = ({type = 'button', children}) => {
+    return ( <button type={type} className='trash-button' onClick={children}></button> );
 }
  
 export default TrashButton;
