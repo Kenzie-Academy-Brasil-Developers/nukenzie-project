e54cc31d3cb696fe90454acd645a89389b131227
Vitor Galdino <httpvittor@gmail.com>
2022-11-16 18:34:42 -0300

    feat: final component structure

---
+++ src/components/List/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import SecondaryButton from '../SecondaryButton';
+
+const List = () => {
+    return ( 
+        <div className="list">
+            <h4>Resumo financeiro</h4>
+            <ul>
+                <li>
+                    <SecondaryButton value='Todos'/>
+                </li>
+                <li>
+                    <SecondaryButton value='Entradas'/>
+                </li>
+                <li>
+                    <SecondaryButton value='Despesas'/>
+                </li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default List;
