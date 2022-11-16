3806965eafc92f5b35c0f8964e14fe9296cd1506
Vitor Galdino <httpvittor@gmail.com>
2022-11-16 15:54:23 -0300

    feat: call defaultInput component

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import DefaultInput from '../DefaultInput';
 
 const Form = () => {
-    return ( 
+    return (
         <>
-        
+            <div className="main-form">
+                <DefaultInput
+                    type='text'
+                    placeHolder='Digite aqui sua descrição'
+                    value='description'
+                    label='Descrição'
+                    helperText='Ex: Compra de roupas'
+                />
+            </div>
         </>
-     );
+    );
 }
- 
+
 export default Form;
