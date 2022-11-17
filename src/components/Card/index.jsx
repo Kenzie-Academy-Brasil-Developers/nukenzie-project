1b7d284b9d5546170b3c147c40de964497615336
Vitor Galdino <httpvittor@gmail.com>
2022-11-16 23:25:27 -0300

    feat: created removeCard function

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,20 +2,28 @@ import React from 'react';
 import TrashButton from '../TrashButton';
 import './index.css';
 
-const Card = ({ data }) => {
+const Card = ({ data, callback }) => {
+
+    const removeCard = (e) => {
+        const elemIndex = data.findIndex((elem) => elem.id === parseInt(e.target.id));
+        const newData = [...data];
+        newData.splice(elemIndex, 1);
+        callback(newData);
+    }
+
     return (
         <>
             <ul className='list-card'>
-                {data.map(({ description, type, value }) => {
+                {data.map(({ description, type, value, id }) => {
                     return (
-                        <li className={type.toLowerCase() == 'entrada' ? 'entry-type' : false}>
+                        <li key={id} className={type.toLowerCase() == 'entrada' ? 'entry-type' : false}>
                             <div className="left-card">
                                 <h4>{description}</h4>
                                 <span>{type}</span>
                             </div>
                             <div className="card-details">
                                 <span>{Math.abs(value).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</span>
-                                <TrashButton />
+                                <TrashButton callback={removeCard} id={id} />
                             </div>
                         </li>
                     );
